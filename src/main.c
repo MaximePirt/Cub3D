87cc4df81ee84b73305266ca2ff436ec43927e5b
MaximePirt <pierrot.maxime.pro@gmail.com>
2024-11-24 08:24:45 +0100

    add todo list

--- src/main.c
+++ src/main.c
@@ -1,25 +1,25 @@
 #include "cube.h"
 
-static void	fill_map_test(t_map *map)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < map->size_y)
-	{
-		j = 0;
-		while (j < map->size_x)
-		{
-			if (i == 0 || i == map->size_y - 1 || j == 0 || j == map->size_x - 1)
-				map->blocks[i][j].type = WALL;
-			else
-				map->blocks[i][j].type = FLOOR;
-			j++;
-		}
-		i++;
-	}
-}
+//static void	fill_map_test(t_map *map)
+//{
+//	int	i;
+//	int	j;
+//
+//	i = 0;
+//	while (i < map->size_y)
+//	{
+//		j = 0;
+//		while (j < map->size_x)
+//		{
+//			if (i == 0 || i == map->size_y - 1 || j == 0 || j == map->size_x - 1)
+//				map->blocks[i][j].type = WALL;
+//			else
+//				map->blocks[i][j].type = FLOOR;
+//			j++;
+//		}
+//		i++;
+//	}
+//}
 
 int	main(int argc, char **argv)
 {
@@ -34,26 +34,26 @@ int	main(int argc, char **argv)
 	}
 	if (parsing_map(argv[1]))
           return (1);
-  map = NULL;
-  if (check_map(map, argv[1]))
+	map = NULL;
+	if (check_map(map, argv[1]))
 	{
       ft_printf("Error: invalid map\n");
       return (1);
 	}
 
-	map = ft_init_map(5, 5);
-	win = ft_init_window();
-
-	load_texture(map->textures->wall_north, "./maps/wall.xpm", win->mlx_ptr);
-	load_texture(map->textures->wall_south, "./maps/wall.xpm", win->mlx_ptr);
-	load_texture(map->textures->wall_east, "./maps/wall.xpm", win->mlx_ptr);
-	load_texture(map->textures->wall_west, "./maps/wall.xpm", win->mlx_ptr);
-	load_texture(map->textures->door, "./maps/wall.xpm", win->mlx_ptr);
-
-	//TODO: render map
-	refresh(win, map);
-	ft_init_keymap(win, map);
-	mlx_loop(win->mlx_ptr);
+//	map = ft_init_map(5, 5);
+//	win = ft_init_window();
+//
+//	load_texture(map->textures->wall_north, "./maps/wall.xpm", win->mlx_ptr);
+//	load_texture(map->textures->wall_south, "./maps/wall.xpm", win->mlx_ptr);
+//	load_texture(map->textures->wall_east, "./maps/wall.xpm", win->mlx_ptr);
+//	load_texture(map->textures->wall_west, "./maps/wall.xpm", win->mlx_ptr);
+//	load_texture(map->textures->door, "./maps/wall.xpm", win->mlx_ptr);
+//
+//	//TODO: render map
+//	refresh(win, map);
+//	ft_init_keymap(win, map);
+//	mlx_loop(win->mlx_ptr);
 
 	return (0);
 }
