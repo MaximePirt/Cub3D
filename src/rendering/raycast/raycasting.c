9a242105db1dea18c0c82abb9e37568360760353
Maxime <pierrot.maxime.pro@gmail.com>
2025-01-18 17:23:52 +0100

    norm raycasting

--- src/rendering/raycast/raycasting.c
+++ src/rendering/raycast/raycasting.c
@@ -6,11 +6,13 @@
 /*   By: mpierrot <mpierrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 04:35:29 by mpierrot          #+#    #+#             */
-/*   Updated: 2025/01/17 14:30:35 by mpierrot         ###   ########.fr       */
+/*   Updated: 2025/01/18 17:22:39 by mpierrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
+
+#define DEG_TO_RAD(angleDegrees) ((angleDegrees)*M_PI / 180.0)
 
 /**
 * TODO: Remove this comment
@@ -19,7 +21,8 @@
 *	On this file, i need to work on the raycasting calcul
 *	We base our raycasting by sending the first ray from the player with
 *	a 0dg angle, then draw them from middle to right, then from left to middle
-*	The struct is double linked, we can imagined it like a circle, middle to right, then left to middle
+*	The struct is double linked, we can imagined it like a circle,
+		middle to right, then left to middle
 *	How proceed :
 *	1. send first ray with 0 angle
 *	2. send ray from (pi/2) - FOV/2
@@ -47,88 +50,115 @@ exemple :
 	longueur = sqrt(1.04)
 	longueur = 1.02
 */
-#define DEG_TO_RAD(angleDegrees) ((angleDegrees) * M_PI / 180.0)
 
-void	process_ray(t_map *map, t_ray *ray, int ray_index) {
-	double add_angle = (ray_index * (FOV / (double)RAYS_COUNT)) - (FOV / 2);
-	double angle = map->player.dir + add_angle;
 
-	double ray_dirX = cos(DEG_TO_RAD(angle));
-	double ray_dirY = sin(DEG_TO_RAD(angle));
-
-	double posX = map->player.x;
-	double posY = map->player.y;
-
-	int mapX = (int)posX;
-	int mapY = (int)posY;
-
-	double deltaDistX = (ray_dirX == 0) ? 1e30 : fabs(1 / ray_dirX);
-	double deltaDistY = (ray_dirY == 0) ? 1e30 : fabs(1 / ray_dirY);
-
-	int stepX, stepY;
-	double sideDistX, sideDistY;
-
-	if (ray_dirX < 0) {
-		stepX = -1;
-		sideDistX = (posX - mapX) * deltaDistX;
-	} else {
-		stepX = 1;
-		sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+static void	struct_fill_forest(t_calcul_ray *calcul)
+{
+	if (calcul->ray_dirX < 0)
+	{
+		calcul->stepX = -1;
+		calcul->sideDistX = (calcul->posX - calcul->mapX) * calcul->deltaDistX;
 	}
-	if (ray_dirY < 0) {
-		stepY = -1;
-		sideDistY = (posY - mapY) * deltaDistY;
-	} else {
-		stepY = 1;
-		sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+	else
+	{
+		calcul->stepX = 1;
+		calcul->sideDistX = (calcul->mapX + 1.0 - calcul->posX)
+			* calcul->deltaDistX;
 	}
-
-	int hit = 0;
-	int side = 0;
-	while (hit == 0) {
-		if (sideDistX < sideDistY) {
-			sideDistX += deltaDistX;
-			mapX += stepX;
-			side = 0;
-		} else {
-			sideDistY += deltaDistY;
-			mapY += stepY;
-			side = 1;
-		}
-		if (map->blocks[mapY][mapX].type != FLOOR) {
-			hit = 1;
-		}
+	if (calcul->ray_dirY < 0)
+	{
+		calcul->stepY = -1;
+		calcul->sideDistY = (calcul->posY - calcul->mapY) * calcul->deltaDistY;
 	}
-
-	double perpWallDist;
-	if (side == 0) {
-		perpWallDist = (mapX - posX + (1 - stepX) / 2) / ray_dirX;
-	} else {
-		perpWallDist = (mapY - posY + (1 - stepY) / 2) / ray_dirY;
+	else
+	{
+		calcul->stepY = 1;
+		calcul->sideDistY = (calcul->mapY + 1.0 - calcul->posY)
+			* calcul->deltaDistY;
 	}
-
-	ray->distance = perpWallDist;
-	ray->x_axis = side == 0 ? posY + perpWallDist * ray_dirY : posX + perpWallDist * ray_dirX;
-	ray->x_axis = ray->x_axis - (int)ray->x_axis;
 }
 
+static void	struct_filler(t_calcul_ray *calcul, t_map *map, int ray_index)
+{
+	calcul->add_angle = (ray_index * (FOV / (double)RAYS_COUNT)) - (FOV / 2);
+	calcul->angle = map->player.dir + calcul->add_angle;
+	calcul->ray_dirX = cos(DEG_TO_RAD(calcul->angle));
+	calcul->ray_dirY = sin(DEG_TO_RAD(calcul->angle));
+	calcul->posX = map->player.x;
+	calcul->posY = map->player.y;
+	calcul->mapX = (int)calcul->posX;
+	calcul->mapY = (int)calcul->posY;
+	calcul->hit = 0;
+	calcul->side = 0;
+	if (calcul->ray_dirX == 0)
+		calcul->deltaDistX = 1e30;
+	else
+		calcul->deltaDistX = fabs(1 / calcul->ray_dirX);
+	if (calcul->ray_dirY == 0)
+		calcul->deltaDistY = 1e30;
+	else
+		calcul->deltaDistY = fabs(1 / calcul->ray_dirY);
+	struct_fill_forest(calcul);
+}
+static void	hit_loop(t_calcul_ray calcul, t_map *map)
+{
+	while (calcul.hit == 0)
+	{
+		if (calcul.sideDistX < calcul.sideDistY)
+		{
+			calcul.sideDistX += calcul.deltaDistX;
+			calcul.mapX += calcul.stepX;
+			calcul.side = 0;
+		}
+		else
+		{
+			calcul.sideDistY += calcul.deltaDistY;
+			calcul.mapY += calcul.stepY;
+			calcul.side = 1;
+		}
+		if (map->blocks[calcul.mapY][calcul.mapX].type != FLOOR)
+			calcul.hit = 1;
+	}
+	return ;
+}
+
+void	process_ray(t_map *map, t_ray *ray, int ray_index)
+{
+	t_calcul_ray	calcul;
+
+	struct_filler(&calcul, map, ray_index);
+	hit_loop(calcul, map);
+	if (calcul.side == 0)
+		calcul.perpWallDist = (calcul.mapX - calcul.posX + (1 - calcul.stepX)
+				/ 2) / calcul.ray_dirX;
+	else
+		calcul.perpWallDist = (calcul.mapY - calcul.posY + (1 - calcul.stepY)
+				/ 2) / calcul.ray_dirY;
+	ray->distance = calcul.perpWallDist;
+	if (calcul.side == 0)
+		ray->x_axis = calcul.posY + calcul.perpWallDist * calcul.ray_dirY;
+	else
+		ray->x_axis = calcul.posX + calcul.perpWallDist * calcul.ray_dirX;
+	ray->x_axis = ray->x_axis - (int)ray->x_axis;
+}
 
 /**
  * @brief Cast all rays for the player's field of view and calculate distances.
  * @param map The map containing ray and block data.
  * @return int Status code (0 for success).
  */
-int give_all_rays(t_map *map)
+int	give_all_rays(t_map *map)
 {
-	int i = 0;
-	t_ray *tmp = map->rays;
+	int		i;
+	t_ray	*tmp;
 
+	i = 0;
+	tmp = map->rays;
 	while (i < RAYS_COUNT)
 	{
 		process_ray(map, tmp, i);
 		tmp = tmp->next;
 		i++;
 	}
-
-	return 0;
+	return (0);
 }
