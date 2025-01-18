744986bfabe2cd04fe8a7f2e279d310fde002e85
CleSucre <47931504+CleSucre@users.noreply.github.com>
2025-01-18 17:33:50 +0100

    rendering fixed (textures not fixed

--- src/rendering/rendering.c
+++ src/rendering/rendering.c
@@ -26,23 +26,24 @@ static void	paste_image_on_screen(t_win *win, t_image *image, t_vector2 pos)
 	}
 }
 
-static void process_ray(t_win *win, t_map *map, t_ray *ray, int ray_index)
+static void process_ray(t_image *img, t_map *map, t_ray *ray, int ray_index)
 {
 	t_image	*texture;
-	int		screen_height;
-	int		screen_width;
 	int		height;
 	int		width;
 	double	angle;
 	int		x;
 	int		y;
 	int		texture_x;
-
-	screen_height = SCREEN_HEIGHT;
-	screen_width = SCREEN_WIDTH;
+	double	corrected_distance;
 
 	double add_angle = (ray_index * (FOV / (double)RAYS_COUNT)) - (FOV / 2);
 	angle = fmod(map->player.dir + add_angle + 360, 360);
+
+	if (angle >= 360)
+		angle -= 360;
+	else if (angle < 0)
+		angle += 360;
 
 	if (angle >= 0 && angle < 90)
 		texture = map->textures->wall_north;
@@ -53,27 +54,31 @@ static void process_ray(t_win *win, t_map *map, t_ray *ray, int ray_index)
 	else
 		texture = map->textures->wall_west;
 
-	height = screen_height / ray->distance;
-	width = fmin(1, screen_width / RAYS_COUNT);
+	double ray_angle = add_angle * M_PI / 180.0;
+	corrected_distance = ray->distance * cos(ray_angle);
+
+	if (corrected_distance < 0.1)
+		corrected_distance = 0.1;
+
+	height = (int)(SCREEN_HEIGHT / corrected_distance);
+	width = fmax(1, SCREEN_WIDTH / RAYS_COUNT);
 
 	texture_x = (int)((ray->x_axis - floor(ray->x_axis)) * texture->width);
 
 	x = ray_index * width;
-	y = (screen_height - height) / 2;
+	y = (SCREEN_HEIGHT - height) / 2;
 
-	// Débogage des valeurs
+	if (width + texture_x > texture->width)
+		width = texture->width - texture_x;
 
-	// Copier les pixels de la texture vers l'écran
-	copy_vertical_pixels(win, texture, ft_vector2(x, y), height, width, texture_x);
+	copy_vertical_pixels(img, texture, ft_vector2(x, y), height, width, texture_x);
 
-	// Dessiner le plafond
-	//draw_rectangle(win, ft_vector2(x, 0), width, y, map->textures->ceiling.r);
+	draw_rectangle(img, ft_vector2(x, 0), width, y, map->textures->ceiling.r);
 
-	// Dessiner le sol
-	//draw_rectangle(win, ft_vector2(x, y + height), width, screen_height - (y + height), map->textures->floor.g);
+	draw_rectangle(img, ft_vector2(x, y + height), width, SCREEN_HEIGHT - (y + height), map->textures->floor.g);
 }
 
-static void render_game(t_win *win, t_map *map)
+static void render_game(t_image *img, t_map *map)
 {
 	t_ray	*ray;
 	int 	i;
@@ -82,7 +87,7 @@ static void render_game(t_win *win, t_map *map)
 	i = 0;
 	while (ray)
 	{
-		process_ray(win, map, ray, i);
+		process_ray(img, map, ray, i);
 		ray = ray->next;
 		i++;
 	}
@@ -96,14 +101,19 @@ static void render_game(t_win *win, t_map *map)
  */
 void	refresh(t_win *win, t_map *map)
 {
-	t_image	*minimap;
-	t_vector2 hand_pos;
+	t_image		*minimap;
+	t_image		*render;
+	t_vector2	hand_pos;
 
 	give_all_rays(map);
 	minimap = draw_minimap(map, win->mlx_ptr);
 	if (!minimap)
 		return;
-	render_game(win, map);
+	render = ft_init_image(win->mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT);
+	render_game(render, map);
+	paste_image_on_screen(win, render, ft_vector2(0, 0));
+	mlx_destroy_image(win->mlx_ptr, render->img_ptr);
+	free(render);
 	paste_image_on_screen(win, minimap, ft_vector2(0, 0));
 	if (map->player.hand_animation_direction)
 		map->player.hand_animation_pos += 1;
@@ -113,6 +123,7 @@ void	refresh(t_win *win, t_map *map)
 		map->player.hand_animation_direction = !map->player.hand_animation_direction;
 	hand_pos = ft_vector2(SCREEN_WIDTH - map->textures->right_hand->width - 10, SCREEN_HEIGHT - map->textures->right_hand->height + 10 + map->player.hand_animation_pos / 10);
 	paste_image_on_screen(win, map->textures->right_hand, hand_pos);
+
 	mlx_put_image_to_window(win->mlx_ptr, win->win_ptr, win->img_ptr, 0, 0);
 	mlx_destroy_image(win->mlx_ptr, minimap->img_ptr);
 	free(minimap);
