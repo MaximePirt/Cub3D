0e4f175f1309340219391486f648be9170d5fa64
Maxime <pierrot.maxime.pro@gmail.com>
2025-01-22 18:23:51 +0100

    finish norm with brush.c, add a file draw.c

--- src/rendering/drawer/brushs.c
+++ src/rendering/drawer/brushs.c
@@ -1,50 +1,42 @@
 #include "cube.h"
 
-void	ft_draw_pixel_on_image(t_image *image, t_vector2 pos, int color)
+static void	draw_loop(t_vector2 delta, t_image *img, t_line line, double steps)
 {
-	if (pos.x < 0 || pos.x >= image->width
-		|| pos.y < 0 || pos.y >= image->height)
-		return ;
-	*(int *)(image->img_data + ((int)pos.x * image->bpp / 8) \
-		+ ((int)pos.y * image->size_line)) = color;
-}
+	t_vector2	current_point;
+	double		i;
+	double		w;
 
-void	ft_draw_pixel_on_screen(t_win *win, t_vector2 pos, int color)
-{
-	*(int *)(win->img_data + ((int)pos.x * win->bpp / 8) \
-		+ ((int)pos.y * win->size_line)) = color;
+	w = -(line.width / 2.0) - 1;
+	while (w++ <= (line.width / 2.0))
+	{
+		i = -1.0;
+		while (i++ <= steps)
+		{
+			current_point.x = line.start.x + (delta.x * i) / steps;
+			current_point.y = line.start.y + (delta.y * i) / steps;
+			if (fabs(delta.x) > fabs(delta.y))
+				ft_draw_pixel_on_image(img, \
+					ft_vector2(current_point.x, current_point.y + w), \
+					line.color);
+			else
+				ft_draw_pixel_on_image(img, \
+					ft_vector2(current_point.x + w, current_point.y), \
+					line.color);
+		}
+	}
 }
 
 void	ft_draw_line(t_image *img, t_line line)
 {
 	t_vector2	delta;
-	t_vector2	current_point;
 	double		steps;
-	double		i;
-	double		w;
 
-	delta.x = line.end.x - line.start.x;
-	delta.y = line.end.y - line.start.y;
+	delta = ft_vector2(line.end.x - line.start.x, line.end.y - line.start.y);
 	if (fabs(delta.x) > fabs(delta.y))
 		steps = fabs(delta.x);
 	else
 		steps = fabs(delta.y);
-	w = -(line.width / 2.0);
-	while (w <= (line.width / 2.0))
-	{
-		i = 0.0;
-		while (i <= steps)
-		{
-			current_point.x = line.start.x + (delta.x * i) / steps;
-			current_point.y = line.start.y + (delta.y * i) / steps;
-			if (fabs(delta.x) > fabs(delta.y))
-				ft_draw_pixel_on_image(img, ft_vector2(current_point.x, current_point.y + w), line.color);
-			else
-				ft_draw_pixel_on_image(img, ft_vector2(current_point.x + w, current_point.y), line.color);
-			i += 1.0;
-		}
-		w += 1.0;
-	}
+	draw_loop(delta, img, line, steps);
 }
 
 int	get_pixel_color(t_image *texture, t_vector2 pos)
